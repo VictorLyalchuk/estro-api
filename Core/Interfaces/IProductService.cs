22e1947c538e639f2fed3e536febed62a53338c8
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2024-09-29 16:08:25 +0300

    admin search

--- Core/Interfaces/IProductService.cs
+++ Core/Interfaces/IProductService.cs
@@ -8,13 +8,13 @@ namespace Core.Interfaces
         Task<List<ProductDTO>?> GetProductByCategoryIdAsync(int id);
         Task<List<ProductDTO>>? GetProductByURLNameAsync(string urlName);
         Task<List<ProductDTO>>? FilterProductsAsync(FilterDTO? filterDTO);
-        Task<List<ProductDTO>> GetProductByPageAsync(int page);
+        Task<List<ProductDTO>> GetProductByPageAsync(int page, string text);
         Task<ProductDTO>? GetProductByIDAsync(int id);
         Task CreateAsync (CreateProductDTO createProductDTO);
         Task DeleteProductByIDAsync(int id);
         Task<ProductDTOEdit>? GetEditProductById(int id);
         Task EditAsync(EditProductDTO editProductDTO);
-        Task <int> ProductQuantity();
+        Task <int> ProductQuantity(string text);
         Task <int> ProductQuantityByFiltersAsync(FilterDTO filterDTO);
     }
 }
